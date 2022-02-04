dc9be9be6e6bcdfe1e0b568e4ba6b91b968c9268
AlfaDinamitaEscuadronLobo <201801300F@utea.edu.pe>
2022-02-04 02:12:25 -0500

    2:12 AM :( y quiero mimir info de la app

---
+++ lib/screens/info_tarea.dart
@@ -0,0 +1,53 @@
+class AppInfoScreen extends StatelessWidget {
+  static String id = 'AppInfoScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        FontAwesomeIcons.arrowLeft,
+                        size: 30.0,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ListTile(
+                      title: Text(
+                          'Desarrollado por:                                            '
+                              'Allende Flores Maria del Carmen                            '
+                              'Ortiz Pachacutec Felix Raul                                       '
+                              'Muñoz Apaza Geidar Omar Renzo                   '
+                              '.                                                     '
+                              'Objetivo de la aplicacion es ser gestor de tareas con recordatorios '),
+                    ),
+                    Divider(
+                      thickness: 2.0,
+                    ),
+                    ListTile(
+                      title: Text('VERSION 0.1'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
