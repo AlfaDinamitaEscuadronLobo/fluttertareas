f920883823b8f4a32c04ff33fbe7e37da4d9964f
AlfaDinamitaEscuadronLobo <201801300F@utea.edu.pe>
2022-02-04 23:57:53 -0500

    modo oscuro activado gaaaa

---
+++ lib/screens/modo_oscuro.dart
@@ -0,0 +1,48 @@
+
+class SettingsScreen extends StatefulWidget {
+  static String id = 'SettingsScreen';
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+
+  bool isDark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  FontAwesomeIcons.arrowLeft,
+                  size: 30.0,
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              SwitchListTile(
+                value: Provider.of<TaskData>(context).currTheme == ThemeData.dark(),
+                title: Text('Aplicar Modo Oscuro'),
+                onChanged: (newVal) {
+                  setState(() {
+                    isDark = newVal;
+                    Provider.of<TaskData>(context, listen: false).toggleTheme();
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
