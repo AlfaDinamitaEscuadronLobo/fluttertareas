ebe740a03ce50bd013a2774510baaf6f087611eb
Maricita12 <201323393e@utea.edu.pe>
2022-02-08 01:20:03 -0500

    septimo commit - carmen

--- lib/screens/editar_tarea.dart
+++ lib/screens/editar_tarea.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/constants.dart';
+import 'package:todoey/models/task.dart';
+import 'package:todoey/models/task_data.dart';
+import 'package:todoey/widgets/option_button.dart';
+
+import '../main.dart'; 
+
 class TaskEditScreen extends StatefulWidget {
   TaskEditScreen({this.task});
 
